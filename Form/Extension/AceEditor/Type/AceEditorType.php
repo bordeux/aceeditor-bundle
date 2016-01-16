5baea5655ab1d89b714af97e577f5249e3ac04f9
Krzysztof Bednarczyk <github@bordeux.net>
2016-01-16 11:25:47 +0100

    Error fix
    Fix error:
    Impossible to access an attribute ("value") on a string variable ("100%") in NorzechowiczAceEditorBundle:Form:div_layout.html.twig at line 11

--- Form/Extension/AceEditor/Type/AceEditorType.php
+++ Form/Extension/AceEditor/Type/AceEditorType.php
@@ -12,11 +12,9 @@
 namespace Norzechowicz\AceEditorBundle\Form\Extension\AceEditor\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\Options;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
@@ -36,31 +34,7 @@ class AceEditorType extends AbstractType
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array(
-            'required' => false,
-            'wrapper_attr' => array(),
-            'width' => 200,
-            'height' => 200,
-            'font_size' => 12,
-            'mode' => 'ace/mode/html',
-            'theme' => 'ace/theme/monokai',
-            'tab_size' => null,
-            'read_only' => null,
-            'use_soft_tabs' => null,
-            'use_wrap_mode' => null,
-            'show_print_margin' => null,
-            'highlight_active_line' => null
-        ));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefined(array('font_size', 'height', 'highlight_active_line', 'mode', 'tab_size',
-            'show_print_margin', 'use_wrap_mode', 'theme', 'width', 'wrapper_attr', 'use_soft_tabs', 'read_only'));
-
+        // Remove id from ace editor wrapper attributes. Id must be generated.
         $wrapperAttrNormalizer = function (Options $options, $aceAttr) {
             if (is_array($aceAttr)) {
                 if (array_key_exists('id', $aceAttr)) {
@@ -88,13 +62,42 @@ class AceEditorType extends AbstractType
             return array('value' => $value, 'unit' => $unit);
         };
 
+        $resolver->setDefaults(array(
+            'required' => false,
+            'wrapper_attr' => array(),
+            'width' => 200,
+            'height' => 200,
+            'font_size' => 12,
+            'mode' => 'ace/mode/html',
+            'theme' => 'ace/theme/monokai',
+            'tab_size' => null,
+            'read_only' => null,
+            'use_soft_tabs' => null,
+            'use_wrap_mode' => null,
+            'show_print_margin' => null,
+            'show_invisibles' => null,
+            'highlight_active_line' => null
+        ));
 
+        $resolver->setAllowedTypes(array(
+            'width' => array('integer', 'string', 'array'),
+            'height' => array('integer', 'string', 'array'),
+            'mode' => 'string',
+            'font_size' => 'integer',
+            'tab_size' => array('integer', 'null'),
+            'read_only' => array('bool', 'null'),
+            'use_soft_tabs' => array('bool', 'null'),
+            'use_wrap_mode' => array('bool', 'null'),
+            'show_print_margin' => array('bool', 'null'),
+            'show_invisibles' => array('bool', 'null'),
+            'highlight_active_line' => array('bool', 'null'),
+        ));
 
-
-
-        $resolver->setNormalizer('wrapper_attr', $wrapperAttrNormalizer);
-        $resolver->setNormalizer('width', $unitNormalizer);
-        $resolver->setNormalizer('height', $unitNormalizer);
+        $resolver->setNormalizers(array(
+            'wrapper_attr' => $wrapperAttrNormalizer,
+            'width'        => $unitNormalizer,
+            'height'       => $unitNormalizer,
+        ));
     }
 
     /**
@@ -116,6 +119,7 @@ class AceEditorType extends AbstractType
                 'use_soft_tabs' => $options['use_soft_tabs'],
                 'use_wrap_mode' => $options['use_wrap_mode'],
                 'show_print_margin' => $options['show_print_margin'],
+                'show_invisibles' => $options['show_invisibles'],
                 'highlight_active_line' => $options['highlight_active_line'],
             )
         );
@@ -126,7 +130,7 @@ class AceEditorType extends AbstractType
      */
     public function getParent()
     {
-        return TextareaType::class;
+        return 'textarea';
     }
 
     /**
